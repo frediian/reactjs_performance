3486e2714ceb57edf893052b36c59d2fed1ff5c0
fredii_an <dev@goshop.ec>
2022-07-05 23:30:38 -0500

    create default contact with and instace of a class

--- src/components/container/contact_list.jsx
+++ src/components/container/contact_list.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { Contact } from "../../models/contact.class";
 import ContactComponent from "../pure/contact";
 
 const ContactListComponent = () => {
-  return (<div>
-    <ContactComponent />
-  </div>);
+
+  const defaultContact = new Contact (
+    'first name',
+    'last name',
+    'email@addres.com',
+    false
+  )
+
+  return (
+    <div>
+      <ContactComponent contact={defaultContact} />
+    </div>
+  );
 };
-
-
 
 export default ContactListComponent;
