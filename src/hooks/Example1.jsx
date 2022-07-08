f8b78a87a8a51dfa73bb3d5d150a5d172f3bc459
fredii_an <dev@goshop.ec>
2022-07-07 21:52:09 -0500

    example 1, buttons

--- src/hooks/Example1.jsx
+++ src/hooks/Example1.jsx
@@ -26,7 +26,15 @@ const Example1 = () => {
 
     return (
         <div>
-            
+            <h1> Example of Hook useState() </h1>
+            <h3>Count : {count}</h3>
+            <h3>Datos de la persona </h3>
+            <h4>Nombre: {person.name} </h4>
+            <h4>Email: {person.email} </h4>
+            <div>
+                <button onClick={addCount} > Add Count </button>
+                <button onClick={changePerson} > Change Person </button>
+            </div>
         </div>
     );
 }
